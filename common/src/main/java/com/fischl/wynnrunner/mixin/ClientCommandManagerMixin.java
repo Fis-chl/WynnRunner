f058662879922738982082c264829d7affe214ea
Fis-chl <lukeeebmusic@gmail.com>
2026-03-03 21:19:48 +0000

    Add back ClientCommandManagerMixin and change version to v0.0.1-alpha
    + ClientCommandManagerMixin re-introduced so /wynnrunner command exists
    + Added description and temporary logo (github profile picture)
    = Change version from 0.0.1 to v0.0.1-alpha to better represent the state of the initial alpha release

--- common/src/main/java/com/fischl/wynnrunner/mixin/ClientCommandManagerMixin.java
+++ common/src/main/java/com/fischl/wynnrunner/mixin/ClientCommandManagerMixin.java
@@ -5,15 +5,8 @@
 package com.fischl.wynnrunner.mixin;
 
 import com.fischl.wynnrunner.commands.WynnrunnerCommand;
-import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.tree.CommandNode;
-import com.mojang.brigadier.tree.RootCommandNode;
 import com.wynntils.core.consumers.commands.ClientCommandManager;
 import com.wynntils.core.consumers.commands.Command;
-import java.util.List;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.SharedSuggestionProvider;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,18 +15,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientCommandManager.class)
 public abstract class ClientCommandManagerMixin {
-    @Final
-    @Shadow
-    private List<Command> commandInstanceSet;
-
-    @Final
-    @Shadow
-    private CommandDispatcher<CommandSourceStack> clientDispatcher;
-
-    @Shadow
-    public abstract void addNode(
-            RootCommandNode<SharedSuggestionProvider> root, CommandNode<? extends SharedSuggestionProvider> node);
-
     @Shadow
     protected abstract void registerCommand(Command command);
 
